0bdad8848c83aaf1a9b598a40d11df09854ada5e
Nitin Singh Negi <nitinnegi71741@gmail.com>
2023-06-08 21:29:29 +0530

    commit test

--- routes/flight.js
+++ routes/flight.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+//This is the comment 
+
 const{getflight, flightPrices} = require('../controllers/flight');
 
 router.route('/').get(getflight);
