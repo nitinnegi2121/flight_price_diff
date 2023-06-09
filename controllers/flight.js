ea8e9589a5dd744706e03228d9f75d0be9847ea7
Nitin Singh Negi <nitinnegi71741@gmail.com>
2023-06-09 21:56:00 +0530

    update

--- controllers/flight.js
+++ controllers/flight.js
@@ -23,7 +23,7 @@ const getflight = async (req, res)=>{
 
 const flightPrices = async(req, res) =>{
   
-  const flight = await Flight.find(ueryObject);
+  const flight = await Flight.find(queryObject);
 
 console.log(
   " ~ file: flight.js ~ line 15 ~ flightPrices ~ req.query",
